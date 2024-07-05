307eac22e49954a50666695e9a7f6e568726cd1f
mprojr <martinprojcs@gmail.com>
2024-07-05 00:20:44 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ export default function Projects() {
   return (
     <div className='group1 sm:h-[15] min-h-[5vh] h-[30vh] p-6 m-4'>
       <h1 className='flex justify-center my-4 py-2 text-gray-950 font-bold lg:text-4xl md:text-3xl sm:text-2xl text-2xl'> Personal Projects</h1>
-      <div className='h-[300px] w-full grid py-6 gap-6 md:grid-cols-4 sm:grid-cols-3 grid-cols-2'>
+      <div className='min-h-[300px] w-full grid py-6 gap-6 md:grid-cols-4 sm:grid-cols-3 grid-cols-2'>
         {projects.map(project => (
           <div key={project.id} className='relative border border-spacing-1 h-[200px] transparent rounded-md overflow-hidden'>
             <Link to={project.detailsPage} className='fill-transparent w-full h-full group2'>
