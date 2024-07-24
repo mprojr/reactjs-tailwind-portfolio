e26641a4157078933242709f6402bb1383273c6c
mprojr <martinprojcs@gmail.com>
2024-07-24 14:41:50 -0500

    Update Proj1.jsx

--- src/pages/Proj1.jsx
+++ src/pages/Proj1.jsx
@@ -10,7 +10,9 @@ function Proj1() {
         {/* Text Section */}
         <div className="relative z-10 p-4">
           <h2 className="text-4xl font-extrabold mb-5 text-custom-green-dark drop-shadow-lg underline decoration-custom-green-light decoration-4">
-            Etsy Website Template
+          <a href='https://timelessrose.netlify.app/' alt='Timeless Rose' target='_blank'>
+              Etsy Website Template
+            </a>
           </h2>
           <p className="text-lg mb-4">
             This Etsy Website Template is a stylish and functional web template designed specifically for Etsy sellers. It helps showcase your products in an elegant manner, making it easier for potential buyers to browse and purchase. The template features a clean design, user-friendly interface, and is fully responsive, ensuring a seamless experience on all devices.
