6574db1369a4170695ca44c9ddde4b8182b53998
mprojr <martinprojcs@gmail.com>
2024-09-12 17:06:43 -0500

    Test 2

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import pfp from '../assets/img/pfp.jpg';
 
 export default function Aboutme() {
-
   const [Typewriter, setTypewriter] = useState(null);
 
   useEffect(() => {
     // Dynamically import `react-simple-typewriter`
-    import('react-simple-typewriter').then((module) => {
-      setTypewriter(() => module.Typewriter);
-    });
+    import('react-simple-typewriter')
+      .then((module) => {
+        setTypewriter(() => module.Typewriter);
+      })
+      .catch((err) => {
+        console.error('Failed to load Typewriter module:', err);
+      });
   }, []);
 
   return (
@@ -18,13 +21,11 @@ export default function Aboutme() {
 
         <div className='m-4 grid grid-col-6'> 
           <img src={pfp} alt="pfp" className='border-2 p-1 border-white col-span-4 w-56 h-56 rounded-full object-cover shadow-lg mx-auto hover:blur-sm duration-500' draggable="false" />
-
-        { /*           <span className='min-h-[100px] rounded-lg bg-black shadow sm:col-span-2'></span>
-        */ }
         </div> 
         <h1 className='uppercase py-2 text-gray-950 font-bold lg:text-4xl md:text-3xl sm:text-2xl text-2xl'>Martin Rodriguez</h1>
+
         <div className='sm:text-lg md:text-xl lg:text-2xl text-gray-300 font-bold p-3 min-h-[40px]' style={{ height: '40px' }}>
-        {Typewriter ? (
+          {Typewriter ? (
             <Typewriter
               words={['Developer and Programmer.']}
               loop={true}
@@ -47,7 +48,9 @@ export default function Aboutme() {
 
           <div className='py-8 grid grid-cols-12 gap-4'>
             <span className='group border-2 border-d bg-gradient-to-t from-[#a3bc8a] to-[#77915f] opacity-85 min-h-[100px] p-4 rounded-xl shadow sm:col-span-7 col-span-12'>
-              <p className='text-md text-black text-center font-extrabold'>Hi, I am <span className=' text-gray-200'>Martin Rodriguez,</span> a passionate <span className=' text-gray-20'>Web Developer and Programmer</span> from <span className=' text-gray-200'>Houston, TX.</span> With a background in <span className=' text-gray-200'>Computer Science,</span> I have honed my skills in various <span className=' text-gray-200 text'>programming languages</span> and technologies to build <span className='text-gray-200 group-hover:text-gray-700 transition duration-300'>robust and scalable </span>applications.</p>
+              <p className='text-md text-black text-center font-extrabold'>
+                Hi, I am <span className='text-gray-200'>Martin Rodriguez,</span> a passionate <span className='text-gray-200'>Web Developer and Programmer</span> from <span className='text-gray-200'>Houston, TX.</span> With a background in <span className='text-gray-200'>Computer Science,</span> I have honed my skills in various <span className='text-gray-200'>programming languages</span> and technologies to build <span className='text-gray-200 group-hover:text-gray-700 transition duration-300'>robust and scalable</span> applications.
+              </p>
             </span>
             <span className='flex flex-center justify-center border-2 border-solid bg-gradient-to-t from-[#a3bc8a] to-[#77915f] min-h-[100px] rounded-xl bg-black shadow sm:col-span-5 col-span-12 p-4'>
               <ul className='grid grid-cols-2 gap-2 text-white text-center font-bold'>
@@ -75,6 +78,5 @@ export default function Aboutme() {
         </div>
       </div>
     </div>
-
-  )
+  );
 }
